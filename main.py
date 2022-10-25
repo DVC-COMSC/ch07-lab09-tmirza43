345f99c0a282b4aa80d2b0b9527e0eb639ca34e9
Talha Mirza <tmirza43@gmail.com>
2022-10-24 20:22:08 -0700

    commit1

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 # ******************************
 # Make your Code
 # ******************************
+
+names = []
+shortest = ''
+longest = ''
+
+for i in range(5):
+  name = str(input("Enter a name: "))
+  names.append(name)
+  if i == 0:
+    shortest = name
+  elif len(name) == len(shortest):
+    if ord(name[0]) < ord(shortest[0]):
+      shortest = name
+  if len(name) > len(longest):
+    longest = name
+  if len(name) < len(shortest):
+    shortest = name
+      
+print(shortest, longest)
